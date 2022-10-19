74074f60124b35ced858b12fe524896fce5e2a0c
Rolando Flores Meza <rolandomeza30@gmail.com>
2022-10-19 12:45:51 -0500

    finalizando eventos en javascript

--- src/assets/two.js
+++ src/assets/two.js
@@ -1,5 +1,20 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('from');
 const btn = document.querySelector('#btnCalcular');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
+const pResult = document.querySelector('#result');
+const body = document.querySelector('body');
+btn.addEventListener('click', (event) => {const sumaInputs = input1.value + input2.value;
 
-btn.addEventListener('click', () => {});
+    pResult.innerText = "Result:  " + sumaInputs;
+},false);
+alert('hola');
+
+const passive = {
+    passive: false
+}
+window.addEventListener('scroll', () => {
+    body.classList.add('rojo')
+    alert('dentro de eventos');
+});
