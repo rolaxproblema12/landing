4b936cb0f8fb51541f13ffb1f357ae405e466f0a
Rolando Flores Meza <rolandomeza30@gmail.com>
2022-10-19 09:54:35 -0500

    first commit

---
+++ src/assets/two.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#btnCalcular');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+
+btn.addEventListener('click', () => {});
